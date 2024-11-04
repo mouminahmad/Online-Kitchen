f6df865255ef55d76e97907c87e40ba240a42656
MOUMIN <offical.moumin@gmail.com>
2024-11-04 14:56:10 +0500

    first commit

---
+++ admin/add-food.php
@@ -0,0 +1,169 @@
+<?php include('partials/menu.php'); ?>
+
+<div class="main-content">
+    <div class="wrapper">
+        <h1>Add Food</h1>
+
+        <br><br>
+
+        <?php 
+            if(isset($_SESSION['upload'])) {
+                echo $_SESSION['upload'];
+                unset($_SESSION['upload']);
+            }
+        ?>
+
+        <form action="" method="POST" enctype="multipart/form-data">
+            <table class="tbl-30">
+
+                <tr>
+                    <td>Title: </td>
+                    <td>
+                        <input type="text" name="title" placeholder="Title of the Food" required>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>Description: </td>
+                    <td>
+                        <textarea name="description" cols="30" rows="5" placeholder="Description of the Food." required></textarea>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>Price: </td>
+                    <td>
+                        <input type="number" name="price" required>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>Cooking Time: </td>
+                    <td>
+                        <input type="text" name="cooking_time" placeholder="e.g. 30 minutes" required>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>Select Image: </td>
+                    <td>
+                        <input type="file" name="image" accept="jpg, jpeg, png">
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>Category: </td>
+                    <td>
+                        <select name="category" required>
+                            <?php 
+                                // Create SQL to get all active categories from database
+                                $sql = "SELECT * FROM tbl_category WHERE active='Yes'";
+                                $res = mysqli_query($conn, $sql);
+                                $count = mysqli_num_rows($res);
+
+                                if($count > 0) {
+                                    while($row = mysqli_fetch_assoc($res)) {
+                                        $id = $row['id'];
+                                        $title = $row['title'];
+                                        echo "<option value='$id'>$title</option>";
+                                    }
+                                } else {
+                                    echo "<option value='0'>No Category Found</option>";
+                                }
+                            ?>
+                        </select>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>Featured: </td>
+                    <td>
+                        <input type="radio" name="featured" value="Yes"> Yes 
+                        <input type="radio" name="featured" value="No" checked> No
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>Active: </td>
+                    <td>
+                        <input type="radio" name="active" value="Yes"> Yes 
+                        <input type="radio" name="active" value="No" checked> No
+                    </td>
+                </tr>
+
+                <tr>
+                    <td colspan="2">
+                        <input type="submit" name="submit" value="Add Food" class="btn-secondary">
+                    </td>
+                </tr>
+
+            </table>
+        </form>
+
+        <?php 
+            if(isset($_POST['submit'])) {
+                // Get the data from Form
+                $title = $_POST['title'];
+                $description = $_POST['description'];
+                $price = $_POST['price'];
+                $cooking_time = $_POST['cooking_time'];
+                $category = $_POST['category'];
+
+                $featured = isset($_POST['featured']) ? $_POST['featured'] : "No";
+                $active = isset($_POST['active']) ? $_POST['active'] : "No";
+
+                // Upload the Image if selected
+                if(isset($_FILES['image']['name'])) {
+                    $image_name = $_FILES['image']['name'];
+
+                    if($image_name != "") {
+                        // Rename the Image
+                        $ext = end(explode('.', $image_name));
+                        $image_name = "Food-Name-" . rand(0000, 9999) . "." . $ext;
+
+                        // Source path and Destination path
+                        $src = $_FILES['image']['tmp_name'];
+                        $dst = "../images/food/" . $image_name;
+
+                        // Upload the food image
+                        $upload = move_uploaded_file($src, $dst);
+
+                        if($upload == false) {
+                            $_SESSION['upload'] = "<div class='error'>Failed to Upload Image.</div>";
+                            header('location:' . SITEURL . 'admin/add-food.php');
+                            die();
+                        }
+                    }
+                } else {
+                    $image_name = ""; // Default value
+                }
+
+                // Insert Into Database
+                $sql2 = "INSERT INTO tbl_food SET 
+                    title = '$title',
+                    description = '$description',
+                    price = $price,
+                    cooking_time = '$cooking_time',
+                    image_name = '$image_name',
+                    category_id = $category,
+                    featured = '$featured',
+                    active = '$active'
+                ";
+
+                // Execute the Query
+                $res2 = mysqli_query($conn, $sql2);
+
+                if($res2 == true) {
+                    $_SESSION['add'] = "<div class='success'>Food Added Successfully.</div>";
+                    header('location:' . SITEURL . 'admin/manage-food.php');
+                } else {
+                    $_SESSION['add'] = "<div class='error'>Failed to Add Food.</div>";
+                    header('location:' . SITEURL . 'admin/manage-food.php');
+                }
+            }
+        ?>
+
+    </div>
+</div>
+
+<?php include('partials/footer.php'); ?>
